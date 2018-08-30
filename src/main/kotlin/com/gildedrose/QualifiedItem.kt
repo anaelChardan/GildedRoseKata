8bcfa139e447c4d344a6c9e63e738a641ea851b3
anaelChardan <anael.chardan@gmail.com>
2018-08-30 09:17:55 +0200

    TMP

--- src/main/kotlin/com/gildedrose/QualifiedItem.kt
+++ src/main/kotlin/com/gildedrose/QualifiedItem.kt
@@ -5,16 +5,43 @@ interface QualifiedItem {
     fun afterADay(): QualifiedItem
 }
 
-data class LegendaryItem(private val item: Item) : QualifiedItem {
+sealed class AbstractItem() : QualifiedItem {
+    abstract val item: Item
+
+    internal abstract fun quality(quality: Int): Quality
+    internal abstract fun build(newItem: Item): QualifiedItem
+
     override fun toItem(): Item = item
-    override fun afterADay(): QualifiedItem {
-        return LegendaryItem(
+    override  fun afterADay(): QualifiedItem {
+        return build(
                 Item(
                         item.name,
                         SellIn(item.sellIn).decrease().sellIn,
-                        LegendaryQuality(item.quality).update(SellIn(item.sellIn)).toInt()
+                        quality(item.quality).update(SellIn(item.sellIn)).toInt()
                 )
         )
+
     }
 
+    override fun toString() = item.toString()
+}
+
+data class IncreasingQualityItem(override val item: Item): AbstractItem() {
+    override fun quality(quality: Int): Quality = IncreasingQuality(quality)
+    override fun build(newItem: Item): QualifiedItem = IncreasingQualityItem(newItem)
+}
+
+data class DecreasingQualityItem(override val item: Item): AbstractItem() {
+    override fun quality(quality: Int): Quality = DecreasingQuality(quality)
+    override fun build(newItem: Item): QualifiedItem = DecreasingQualityItem(newItem)
+}
+
+data class LegendaryQualityItem(override val item: Item) : AbstractItem() {
+    override fun quality(quality: Int): Quality = LegendaryQuality(quality)
+    override fun build(newItem: Item): QualifiedItem = LegendaryQualityItem(newItem)
+}
+
+data class SpecialQualityItem(override val item: Item) : AbstractItem() {
+    override fun quality(quality: Int): Quality = SpecialQuality(quality)
+    override fun build(newItem: Item): QualifiedItem = SpecialQualityItem(newItem)
 }
--- src/main/kotlin/com/gildedrose/Quality.kt
+++ src/main/kotlin/com/gildedrose/Quality.kt
@@ -24,41 +24,59 @@ sealed class AbstractQuality constructor(max: Int): Quality {
 }
 
 data class DecreasingQuality(override val quality: Int): AbstractQuality(50) {
-    override fun update(sellByDate: SellIn): Quality = DecreasingQuality(if (quality >= 1) (quality - 1) else 0)
+    override fun update(sellByDate: SellIn): Quality{
+        if (sellByDate.passed()) {
+            return DecreasingQuality(if(quality >= 2) (quality - 2) else 0)
+        }
+
+        return DecreasingQuality(if (quality >= 1) (quality - 1) else 0)
+    }
+}
+
+data class ConjuredQuality(override val quality: Int): AbstractQuality(50) {
+    override fun update(sellByDate: SellIn): Quality{
+        if (sellByDate.passed()) {
+            return DecreasingQuality(if(quality >= 4) (quality - 4) else 0)
+        }
+
+        return DecreasingQuality(if (quality >= 2) (quality - 2) else 0)
+    }
+
 }
 
 data class IncreasingQuality(override val quality: Int): AbstractQuality(50) {
     override fun update(sellByDate: SellIn): Quality = IncreasingQuality(if (quality < maximum) (quality + 1) else maximum)
 }
 
-data class BackstageQuality(override val quality: Int): AbstractQuality(50) {
+data class SpecialQuality(override val quality: Int): AbstractQuality(50) {
     override fun update(sellByDate: SellIn): Quality {
         if (sellByDate.passed()) {
-            return BackstageQuality(0)
+            return SpecialQuality(0)
         }
 
         if (sellByDate.restAtMax(5)) {
             if ((quality * 3) > maximum)
-                return BackstageQuality(maximum)
+                return SpecialQuality(maximum)
 
-            return BackstageQuality(quality * 3)
+            return SpecialQuality(quality * 3)
         }
 
         if (sellByDate.restAtMax(10)) {
             if ((quality * 2) > maximum)
-                return BackstageQuality(maximum)
+                return SpecialQuality(maximum)
 
-            return BackstageQuality(quality * 2)
+            return SpecialQuality(quality * 2)
         }
 
-        return BackstageQuality(if (quality >= 1) (quality - 1) else 0)
+        return SpecialQuality(if (quality >= 1) (quality - 1) else 0)
     }
 }
 
-data class LegendaryQuality(override val quality: Int): AbstractQuality(80) {
-    override fun update(sellByDate: SellIn): Quality = LegendaryQuality(if (quality < maximum) (quality + 1) else maximum)
+class LegendaryQuality(private val quality: Int = 80): Quality {
+    override fun toInt(): Int = quality
+    override fun update(sellByDate: SellIn): Quality = this
 }
 
 
-class TooBigQualityException(val max: Int): Exception("Quality cannot be upper than " + max.toString())
+class TooBigQualityException(max: Int): Exception("Quality cannot be upper than " + max.toString())
 class TooLowQualityException(): Exception("Quality cannot be negative")
